9aa9e9fac76421a9c4154899ad3992b952dfbbdf
Lukecb14 <114123793+Lukecb14@users.noreply.github.com>
2024-07-21 19:11:14 -0400

    Removed unnecessary styling

--- src/components/GameplanFooter.jsx
+++ src/components/GameplanFooter.jsx
@@ -24,7 +24,7 @@ export default function Footer() {
             <div class="col mb-3">
                 <h5>Contact</h5>
                 <ul class="nav flex-column">
-                    <li class="nav-item mb-2" style={{display:'flex', justifyContent:'center'}}>
+                    <li class="nav-item mb-2">
                         <a href="mailto:info@gameplansports.xyz" class="nav-link p-0 text-body-secondary">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                                 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414zM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586zm3.436-.586L16 11.801V4.697z"/>
