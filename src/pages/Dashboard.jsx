a6ed83905e007de0e88f524b9796590de5cf476c
Lukecb14 <114123793+Lukecb14@users.noreply.github.com>
2024-07-21 17:41:20 -0400

    Updated the upcoming games tab to be more cohesive

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,50 +4,71 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
 
 export default function Dashboard() {
     return (
         <div className='px-5' style={{ minHeight: "100vh" }}>
             <NavbarLoggedIn />
             <h1 className='sectionTitle'>Upcoming games and events:</h1>
-            <Container>
-                <Row className='eventBoxRows'>
-                    <Col className='col-6'>
-                        <div className='eventBoxStyle'>
-                            <p className='eventTextStyle'>You (Ottawa Senators) @ Edmonton Oilers</p>
-                            <hr className='eventLineStyle'/>
-                            <p className='eventTextStyle'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</p>
-                            <p className='eventTextStyle'>Today, April 29th, 7:00 pm</p>
-                        </div>
-                    </Col>
-                    <Col className='col-6'>
-                        <div className='eventBoxStyle'>
-                            <p className='eventTextStyle'>You (Ottawa Senators) @ Edmonton Oilers</p>
-                            <hr className='eventLineStyle'/>
-                            <p className='eventTextStyle'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</p>
-                            <p className='eventTextStyle'>Today, April 29th, 7:00 pm</p>
-                        </div>
-                    </Col>
-                </Row>
-                <Row className='eventBoxRows'>
-                    <Col>
-                        <div className='eventBoxStyle'>
-                            <p className='eventTextStyle'>You (Ottawa Senators) @ Edmonton Oilers</p>
-                            <hr className='eventLineStyle'/>
-                            <p className='eventTextStyle'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</p>
-                            <p className='eventTextStyle'>Today, April 29th, 7:00 pm</p>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className='eventBoxStyle'>
-                            <p className='eventTextStyle'>You (Ottawa Senators) @ Edmonton Oilers</p>
-                            <hr className='eventLineStyle'/>
-                            <p className='eventTextStyle'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</p>
-                            <p className='eventTextStyle'>Today, April 29th, 7:00 pm</p>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+            <div className="container">
+                <Table bordered striped classname='statsTable' style={{border:'black'}}>
+                    <thead>
+                        <tr>
+                        <th className='tableHeadItem'>Playing</th>
+                        <th className='tableHeadItem'>Location</th>
+                        <th className='tableHeadItem'>Time</th>
+                        <th className='tableHeadItem'>Previous score</th>
+                        <th className='tableHeadItem'>Additional info</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>You (Ottawa Senators) @ Edmonton Oilers</td>
+                            <td><a href='https://maps.app.goo.gl/qeP5ivGTjhhVRuQH6' target='_blank' className='evenetLocationLink'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</a></td>
+                            <td>Today, July 23th, 7:00pm</td>
+                            <td>3-4 (L)</td>
+                            <td>There's a bar accross the street if we want to stop by afterwards...</td>
+                        </tr>
+                        <tr>
+                            <td>You (Ottawa Senators) vs Toronto Maple leafs</td>
+                            <td><a href='https://maps.app.goo.gl/ZiQSXe3o68s83xLJ8' target='_blank' className='evenetLocationLink'>Barbara Ann Scott Arena, 2250 Torquay Ave, Ottawa, ON</a></td>
+                            <td>Friday, July 26th, 8:30pm</td>
+                            <td>6-0 (W)</td>
+                            <td>This team is not great, should be easy.</td>
+                        </tr>
+                        <tr>
+                            <td>You (Ottawa Senators) @ Edmonton Oilers</td>
+                            <td><a href='https://maps.app.goo.gl/qeP5ivGTjhhVRuQH6' target='_blank' className='evenetLocationLink'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</a></td>
+                            <td>Today, July 23th, 7:00pm</td>
+                            <td>3-4 (L)</td>
+                            <td>There's a bar accross the street if we want to stop by afterwards...</td>
+                        </tr>
+                        <tr>
+                            <td>You (Ottawa Senators) vs Toronto Maple leafs</td>
+                            <td><a href='https://maps.app.goo.gl/ZiQSXe3o68s83xLJ8' target='_blank' className='evenetLocationLink'>Barbara Ann Scott Arena, 2250 Torquay Ave, Ottawa, ON</a></td>
+                            <td>Friday, July 26th, 8:30pm</td>
+                            <td>6-0 (W)</td>
+                            <td>This team is not great, should be easy.</td>
+                        </tr>
+                        <tr>
+                            <td>You (Ottawa Senators) @ Edmonton Oilers</td>
+                            <td><a href='https://maps.app.goo.gl/qeP5ivGTjhhVRuQH6' target='_blank' className='evenetLocationLink'>J.A. Dulude Arena, 941 Clyde Ave, Ottawa, ON</a></td>
+                            <td>Today, July 23th, 7:00pm</td>
+                            <td>3-4 (L)</td>
+                            <td>There's a bar accross the street if we want to stop by afterwards...</td>
+                        </tr>
+                        
+                        
+                    </tbody>
+                </Table>
+                <a href="#/dashboard" style={{textAlign:'left', textDecoration:'none', color:'black'}}>
+                See 5 more events
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 2 16 14">
+                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"></path>
+                </svg>
+              </a>
+            </div>
 
             <h1 className='sectionTitle'>Your team stats:</h1>
             <div className="container">
